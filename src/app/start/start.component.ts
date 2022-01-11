99786f6fdcb9e59a3cd8c71cc94e217dc95871e7
Gengiskan <91074795+ForMyFreedom@users.noreply.github.com>
2022-01-10 23:28:41 -0300

    End the base of elemental functionality

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -3,6 +3,7 @@ import {CharactersApiService} from '../service/characters-api.service'
 import { combineAll, Observable, EMPTY, empty, of, combineLatest, toArray, take } from 'rxjs';
 import { Character } from '../models/character.model';
 import { Data } from '../models/data.model';
+import { Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-start',
@@ -12,22 +13,52 @@ import { Data } from '../models/data.model';
 export class StartComponent implements OnInit {
 
   data: Data;
-  characters: Character[];
-  sortedCharacter: number;
-  characterIdsList: number[];
+  characters: Character[] = [];
+  sortedCharacter: number = 0;
+  characterIdsList: number[] = [];
   user: CharactersApiService;
-  gameStart: boolean;
+  points: number = 0;
+  trys: number = 0;
 
+  gameStart: boolean = false;
+  answerMade: boolean = false;
+  answerResponse: string = "";
+
+  GOAL = 2;
   ALPHABET = "abcdefghijklmnopqrstuvwxyz";
   ORDERS = ["", "-"];
+<<<<<<< Updated upstream
+=======
+  DEFAULT_BLOCK = "http://i.annihil.us/u/prod/marvel/i/mg/";
+  BLOCKED_URIS = ["b/40/image_not_available", "c/e0/4ce59d3a80ff7", "i/mg/6/10/4c003937c9ba4", "i/mg/6/50/4dd531d26079c", "i/mg/6/60/535febc427605", "i/mg/b/c0/52b0d25c3dbb9", "i/mg/6/b0/4ed7bd3756650"];
+  //BLOCKED_URIS IS A TEMPORARY SOLUCTION [Issue #1]
+>>>>>>> Stashed changes
 
   constructor(private _user: CharactersApiService) {
     this.user = _user;
     this.data = Data.prototype;
+<<<<<<< Updated upstream
     this.characterIdsList = [];
     this.sortedCharacter = 0;
     this.characters = [];
     this.gameStart = false;
+=======
+
+    for (let i = 0; i < this.BLOCKED_URIS.length; i++) {
+      this.BLOCKED_URIS[i] = this.DEFAULT_BLOCK + this.BLOCKED_URIS[i];
+    }
+>>>>>>> Stashed changes
+  }
+
+  startData() {
+    this.characters = [];
+    this.sortedCharacter = 0;
+    this.characterIdsList = [];
+    this.points = 0;
+    this.trys = 0;
+    this.gameStart = false;
+    this.answerMade = false;
+    this.answerResponse = "";
   }
 
   ngOnInit(): void {
@@ -35,11 +66,42 @@ export class StartComponent implements OnInit {
   }
 
 
-  on_click(): void {
+  start(): void {
+    if (this.winGame()) {
+      this.startData();
+      return;
+    }
+
+    this.answerMade = false;
     this.getSuficientData();
     this.characters = this.selectAmountOfCharacters(4);
     this.sortedCharacter = this.getRandomNumber(4);
     this.gameStart = true;
+
+  }
+
+  choose_character(id: number): void {
+    if (this.answerMade == true) return;
+
+    this.answerMade = true;
+    this.trys++;
+
+    if (id == this.sortedCharacter) {
+      this.answerResponse = "Você acertou!";
+      this.points++;
+    } else {
+      this.answerResponse = "Você errou...";
+    }
+
+    if (this.winGame()) {
+      this.answerResponse = `Você venceu com ${this.trys} tentativas`;
+    }
+  }
+
+
+
+  winGame(): boolean {
+    return this.points >= this.GOAL;
   }
 
 
